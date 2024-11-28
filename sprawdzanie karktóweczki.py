b912f872ac3efdcd13c6594de3a0b6cb37b7a328
BLCXD <95041805+BLCXD@users.noreply.github.com>
2024-11-28 09:13:11 +0100

    Add files via upload

---
+++ sprawdzanie karktóweczki.py
@@ -0,0 +1,9 @@
+# Tutaj pisz swój kod, młody padawanie ;-)
+a = float(input("Podaj 1 bok"))
+b = float(input("Podaj 2 bok"))
+c = float(input("Podaj 3 bok"))
+
+if a==b or a==c or b==c or c==a:
+    print("Równoboczny")
+else:
+    print("nie")
